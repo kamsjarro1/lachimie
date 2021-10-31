2d2fa3fa52ffe73ce7e527effd498b81e8ae8bac
Kamoga Jarrod <kamsjarros@gmail.com>
2021-10-31 21:34:39 +0300

    create a new git repository

---
+++ kamse/lachimie.py
@@ -0,0 +1,16 @@
+numb = int(input())
+def is_odd(numb):
+    if(numb%2==0):
+        print("False") 
+    else:
+        print("True")    
+print("is_odd(numb)")        
+
+
+def sum_numb(num1,num2):
+    add = num1 + num2;
+    return add;
+num1 = int(input())    
+num2 = int(input())
+
+
